4f78ef64114ddf2386e836363af337e76bfe5ef1
Green, Tal (tg4920) <tg4920@intl.att.com>
2018-10-17 10:59:10 +0300

    Add custom expected condition

--- src/main/java/selenium/_11_waits/Waits.java
+++ src/main/java/selenium/_11_waits/Waits.java
@@ -1,14 +1,12 @@
 package selenium._11_waits;
 
+import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
@@ -33,10 +31,13 @@ public class Waits {
         WebDriverWait wait10Sec = new WebDriverWait(driver, 10); // Time is in seconds
 
 
-        wait10Sec.until(ExpectedConditions.elementToBeClickable(By.id("SomeID")));
-        wait10Sec.until(ExpectedConditions.urlContains("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"));
+//        wait10Sec.until(ExpectedConditions.elementToBeClickable(By.id("SomeID")));
+//        wait10Sec.until(ExpectedConditions.urlContains("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"));
+//
+//        wait10Sec.until(myElementIsFound(By.id("gh-ac1"))).sendKeys("Hello");
 
-        wait10Sec.until(myElementIsFound(By.id("gh-ac1"))).sendKeys("Hello");
+        String textFound = wait10Sec.until(valueIsNotEmpty(By.id("gh-ac")));
+        System.out.println(textFound);
 
     }
 
@@ -49,6 +50,20 @@ public class Waits {
 
             public String toString() {
                 return "My element is found expected condition";
+            }
+        };
+    }
+
+    private static ExpectedCondition<String> valueIsNotEmpty(By by) {
+        return new ExpectedCondition<String>() {
+            @NullableDecl
+            @Override
+            public String apply(@NullableDecl WebDriver driver) {
+                String elementText = driver.findElement(by).getAttribute("value");
+                if (elementText != null && !elementText.trim().isEmpty()) {
+                    return elementText;
+                }
+                return null;
             }
         };
     }
